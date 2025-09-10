ad4e723c926a80c11e077e266187cc76ad515251
keemsisi <keemsisi@gmail.com>
2025-09-10 11:34:34 +0100

    updated codes

--- src/appui/Appui.java
+++ src/appui/Appui.java
@@ -15,7 +15,7 @@ public class Appui {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here and make it clean.
+        // TODO: code application logic here.
     }
     
 }
